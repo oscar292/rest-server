b0073fd8ca4dc252a5cf34bcc1755ceb17b667b4
OscarTorres20 <oseduar123@gmail.com>
2023-03-19 23:13:57 -0500

    configuracion de variables de entorno

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || '3434';
         this.usuariosRoutesPath = '/api/usuarios';
 
         //Middleares
